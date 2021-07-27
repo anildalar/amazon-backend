799b4ec2eb0dbc9036e25ffa6a6989c98bf1b091
anildalar <dollorinfotech@gmail.com>
2021-07-27 09:46:18 +0530

    video 2 - How to receive form data in node js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ var express = require('express')
 //const obj = library()
 const app = express()
 
+//Middleware
+app.use(express.json())
+
 let port = 5000;
 
 //obj.method()
@@ -19,6 +22,17 @@ app.get('/getmyname',(req,res)=>{
 app.post('/getmymission',function(req,res){
     res.send('Our Mission Mission to educate student for coding');
 });
+
+app.post('/getuserinfo',(req,res)=>{
+    let firstname = req.params.fname;
+    console.log(firstname);
+   //HTTP Status Code
+   //res.status(200).json({ "username":  req.body.uname });
+   res.status(200).json({"firstname":firstname});
+
+});
+
+
 
 app.listen(port,()=>{  //Fat Arrow Function to create cbfn
     console.log(`The server is running on Port ${ port } `);
