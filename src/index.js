d99ecfcfcba7041945da4f524b9239ce0de99f2b
anildalar <dollorinfotech@gmail.com>
2021-08-16 07:54:26 +0530

    Express Validator

--- src/index.js
+++ src/index.js
@@ -1,82 +1,40 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+    const express = require('express');
+    const app = express();
 
-const env = require('dotenv');
-env.config();
+    const env = require('dotenv');
+    env.config();
 
+    //import someting from somelibary  mjs
+    // const { a,b,c,d,e } = require();     
+    //const { nameimport1,namedimport2,..... } = require('somelibary');      cjs
 
-//IMport 
-const jwt = require('jsonwebtoken');
+    const { body, validationResult } = require('express-validator');
 
-//Middleware
-app.use(express.json());
+    app.use(express.json());
 
-//connect
-//mongodb+srv://admin123:admin123@amazon-cluster.qnz6h.mongodb.net/AMAZON-BACKEND?retryWrites=true&w=majority
-mongoose.connect('mongodb+srv://admin123:admin123@amazon-cluster.qnz6h.mongodb.net/AMAZON-BACKEND?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+    //app.method('route','mwfn1','mwfn2','cbfn');
+    app.post('/user',
+    // username must be an email
+   // body('username').isEmail(),
+    // password must be at least 5 chars long
+    body('password').isLength({ min: 5 }),
+    (req,res)=>{
 
+        const errors = validationResult(req);
 
-//import db from mongoose.connection
+        if (!errors.isEmpty()) {
+            //Data is invalid
+            return res.status(400).json({ errors: errors.array() });
+        }
+        //Data is valid
+        res.status(200).send({
+            msg:'ok',
+            d1:req.body
+        })
 
-const db = mongoose.connection;
+       
+    });
 
-//object.method();
-/**
- *  class Jwt{
- *      //1. Property
- * 
- *      //2. COnstructor
- * 
- *      //3. Method
- *      function sign(obj,secret){
- * 
- *          return '';
- *      }
- *      
- *  }
- * 
- * let obj = new ClassName();
- * let jwt = new Jwt();
- * 
- * const token = jwt.sign({p:v,p:v,p:v},secreate)
- * 
- */
-var secretKey = 'OKLABSTRAINGISTHEBEST';
-var token = jwt.sign({_id:"6111ee58d644c03dfc6973e0",name:'Anil'},secretKey,{expiresIn:20});
-
-console.log(token);
-
-try{
-    setTimeout(()=>{
-        var decoded = jwt.verify(token,secretKey);
-        console.log(decoded);
-    },10000);
-}catch(err){
-    console.log('Expired '+err);
-}
-
-//something.on('event',cbfn) is used for listining an event
-
-db.on('error',console.error.bind(console, 'connection error:'));
-db.once('open',()=>{
-    console.log('Connected');
-});
-
-
-
-
-
-
-
-//const studentRoute = require('./routes/studentRoute');
-
-//app.use('/api',studentRoute);
-
-
-app.listen(process.env.PORT,()=>{
-    console.log(`The server is running on port ${process.env.PORT}  `);
-});
+    app.listen(process.env.PORT,()=>{
+        console.log(`The server is running on port ${process.env.PORT}  `);
+    });
