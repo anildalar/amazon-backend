5d450fe0b604e9f47abba9b522bc1cfbefdde624
anildalar <dollorinfotech@gmail.com>
2021-08-19 08:38:01 +0530

    Video 17 - Bcrypt

--- src/index.js
+++ src/index.js
@@ -1,100 +1,151 @@
-    const express = require('express');
-    const app = express();
-    const env = require('dotenv');
-    env.config();
+const express = require('express');
+const app = express();
+const env = require('dotenv');
+env.config();
 
-    const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-    app.use(express.json());
 
-    //Deconstruct /Import
-   // const { Schema } = mongoose;
+app.use(express.json());
 
-    //console.log(process.env.MONGO_USER);
-   // console.log(process.env.MONGO_PASS);
-   // console.log(process.env.MONGO_DB);
-    //console.log(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@amazon-cluster.qnz6h.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`);
 
-    mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@amazon-cluster.qnz6h.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+const bcrypt = require('bcrypt');
 
-    const db = mongoose.connection;
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@amazon-cluster.qnz6h.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function () {
-        console.log('Connected');
-    });
+const db = mongoose.connection;
 
-    const userSchema = new mongoose.Schema({
-        firstName:{
+//const { Schema } = require('mongoose');
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Connected');
+});
+
+
+const userSchema = new mongoose.Schema({
+    email: {
             type:String,
             required:true
         },
-        lastName:{
-            type:String
-        },
-        email:{
+    hash_pass:{
             type:String,
             required:true
-        },
-        password:{
-            type:String,
-            required:true
-        },
-        mobileno:{
-            type:String
         }
+});
+
+
+const UserModel = mongoose.model('Users',userSchema);
+
+
+
+//1. Technique
+
+/*
+const hashed_pass = bcrypt.hash('123',10);
+
+console.log(hashed_pass);
+
+hashed_pass.then(function(hashed_pass){
+    console.log(hashed_pass);
+}).catch(function(e){
+
+}).finally();
+*/
+/*
+bcrypt.hash('123',10).then(function(d){
+    console.log(d);
+}).catch(function(e){}).finally(function(){});
+
+bcrypt.hash('123',1).then(function(d){
+    console.log(d);
+}).catch(function(e){}).finally(function(){});
+*/
+
+
+
+app.post('/generatepassword',(req,res)=>{
+
+    const hash = bcrypt.hashSync(req.body.password,10);
+    //console.log(hash);
+
+    //console.log(req.body.password);
+    //Create a Document
+    const userObj = new UserModel({
+        email:req.body.email,
+        hash_pass:hash
     });
 
-    const UserModel = mongoose.model('User',userSchema);
-
-    app.post('/api/register',(req,res)=>{
-
-        //Destructure 
-        const { fn,ln,eml,pwd,mobno } = req.body;
-
-        //console.log(firstName);
-        //console.log(req.body);
-
-        //In Registeration Process first you need to find if the user is already exits
-
-        UserModel.findOne({email:eml}).then((d)=>{
-            if(d){
-                res.status(400).json({
-                    msg:"Failed - User Already Exists"
-                });
-            }else{
-                //User not exits
-                UserModel.create({ firstName:fn,lastName:ln,email:eml,password:pwd,mobileno:mobno }).then((d)=>{
-                    //Success
-                    res.status(200).json({
-                        msg:"OK - Registeration is successfull"
-                    });
-                }).catch((e)=>{
-                    res.status(400).json(e);
-                });
-            }
-        }).catch(function(e){
-            res.status(400).json(e);
-        }).finally();
-        /*
-        exec((e,d)=>{
-            
+    userObj.save().then((d)=>{
+        //Success
+        console.log('saved');
+        res.status(200).json({
+            d:"ok",
+            'hashed_password':hash
         });
-        */
-    });
+    }).catch((e)=>{
+        res.status(400).json(e);
+    }).finally();
+});
 
-   
+app.post('/checkpassword',(req,res)=>{
 
-    //const kitty = new Cat({ name: 'Zildjian' });
-   // kitty.save().then(() => console.log('meow'));
+    let pp = req.body.password;
+
+    // /bcrypt.compareSync(myPlaintextPassword, hash);
+    if(bcrypt.compareSync(req.body.password, '$2b$10$SRcLLiYqsWlASsyK01QmDuQ76KsUMd.fohOqP2ms7qxzRiAsh40z2')){
+        res.status(200).send({
+            msg:"Password Match"
+        });
+    }else{
+        res.status(403).send({
+            msg:"Password does not Match"
+        });
+    }
+
+
+});
+
+
+/*
+ 
 
 
 
 
+// define a schema
 
-    app.listen(process.env.PORT,()=>{
-        console.log(`The server is running on port ${process.env.PORT}  `);
-    });
+
+//schema.virtual('virtualfieldname').get(function(){}).set(function(){});
+//schema.virtual('virtualfieldname').set(function(){});
+//schema.method();
+personSchema.virtual('fullname').get(function(){
+    return this.name +' '+ this.lastname;
+});
+
+//Let Create a Model // // compile our model
+const PersonModel = mongoose.model('Person',personSchema);
+
+//Create a Document
+const personObj = new PersonModel({
+    name:"A",
+    lastname:"B"
+});
+
+personObj.save().then(()=>{
+    console.log('saved');
+}).catch().finally();
+
+console.log(personObj.name +" "+ personObj.lastname);
+
+let x = personObj.fullname;
+
+console.log(x);
+*/
+
+app.listen(process.env.PORT,()=>{
+    console.log(`The server is running on port ${process.env.PORT}  `);
+});
